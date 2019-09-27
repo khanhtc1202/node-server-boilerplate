04d482d6ae28c3c6b6a8c1ebfafc86bc42c0a67c
khanhtc1202 <khanhtc1202@gmail.com>
2019-09-28 01:34:45 +0900

    add catch all

--- router.js
+++ router.js
@@ -10,4 +10,8 @@ const router = express.Router();
 
 router.get('/ping', systemCtrl.ping.bind(systemCtrl));
 
+router.get('*', (req, res) => { res.status(404).contentType("text/plain").end('Invalid GET request') });
+router.post('*', (req, res) => { res.status(404).contentType("text/plain").end('Invalid POST request') });
+router.delete('*', (req, res) => { res.status(404).contentType("text/plain").end('Invalid DELETE request') });
+
 module.exports = router;
